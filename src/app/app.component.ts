a83a4be16383010d009d816cd0fe06f8c558c4d7
JimmyHo <jiaming0708@gmail.com>
2021-06-06 21:21:57 +0800

    feat: split two steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
-import { AfterViewInit, Component, ElementRef, SimpleChanges, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
+import { WorkingStep } from './models/working-step';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent {
+  WorkingStep = WorkingStep;
+  currentStep = WorkingStep.Upload;
+
   onUploaded(event: string[]) {
     console.log('123', event);
   }
 
-  ngAfterViewInit() {
+  changeStep(newStep: WorkingStep) {
+    this.currentStep = newStep;
   }
 }
---
+++ src/app/models/working-step.ts
@@ -0,0 +1,4 @@
+export enum WorkingStep {
+  Upload,
+  Choose,
+}
